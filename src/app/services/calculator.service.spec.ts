87f22bc11396cca6875f62cd7a0a41c6f9053a8d
sajjadmaneshi <maneshisajjad@gmail.com>
2024-10-05 11:31:07 +0330

    implement jasmine spy

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -8,18 +8,22 @@ describe("CalculatorService",()=>{
   it("should add 2 numbers",()=>{
 
     const logger=new LoggerService();
+    spyOn(logger,"log");
     const calculator=new CalculatorService(logger);
     const result=calculator.add(2,5);
     expect(result).withContext('should be').toBe(7)
-
+    expect(logger.log).toHaveBeenCalledTimes(1);
+    // expect(logger.log).withContext('should be called').toHaveBeenCalledTimes(1);
   });
 
   it("should subtract 2 numbers",()=>{
 
-     const logger=new LoggerService();
+     // const logger=new LoggerService();
+     const logger =jasmine.createSpyObj('LoggerService',["log"]);
     const calculator=new CalculatorService(logger);
     const result=calculator.subtract(3,2);
-    expect(result).withContext('should be').toBe(1)
+    expect(result).withContext('should be').toBe(1);
+    expect(logger.log).toHaveBeenCalledTimes(1);
 
 
   })
