f4ff99dc23d35241f7e718c601b7ddec5a385123
sajjadmaneshi <maneshisajjad@gmail.com>
2024-10-05 11:34:01 +0330

    beforeEach

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -3,13 +3,20 @@ import {LoggerService} from "./logger.service";
 
 
 describe("CalculatorService",()=>{
+let calculator:CalculatorService,
+  logger:any;
 
+  beforeEach(()=>{
+      console.log("Before each called");
+     logger =jasmine.createSpyObj('LoggerService',["log"]);
+     calculator=new CalculatorService(logger);
+  })
 
   it("should add 2 numbers",()=>{
 
-    const logger=new LoggerService();
-    spyOn(logger,"log");
-    const calculator=new CalculatorService(logger);
+    // const logger=new LoggerService();
+    // spyOn(logger,"log");
+    // const calculator=new CalculatorService(logger);
     const result=calculator.add(2,5);
     expect(result).withContext('should be').toBe(7)
     expect(logger.log).toHaveBeenCalledTimes(1);
@@ -19,8 +26,8 @@ describe("CalculatorService",()=>{
   it("should subtract 2 numbers",()=>{
 
      // const logger=new LoggerService();
-     const logger =jasmine.createSpyObj('LoggerService',["log"]);
-    const calculator=new CalculatorService(logger);
+    //  const logger =jasmine.createSpyObj('LoggerService',["log"]);
+    // const calculator=new CalculatorService(logger);
     const result=calculator.subtract(3,2);
     expect(result).withContext('should be').toBe(1);
     expect(logger.log).toHaveBeenCalledTimes(1);
