707f18c0a8f2db8fcb3c04168ed4b9b068405cbe
sajjadmaneshi <maneshisajjad@gmail.com>
2024-10-05 11:38:31 +0330

    best practice

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -2,44 +2,38 @@ import {CalculatorService} from "./calculator.service";
 import {LoggerService} from "./logger.service";
 import {TestBed} from "@angular/core/testing";
 
-
 describe("CalculatorService",()=>{
-let calculator:CalculatorService,
-  logger:any;
+
+  let calculator:CalculatorService,
+    logger:any;
+
 
   beforeEach(()=>{
-      console.log("Before each called");
-     logger =jasmine.createSpyObj('LoggerService',["log"]);
-     TestBed.configureTestingModule({
-       providers:[CalculatorService,{provide: LoggerService, useValue:logger}]
-     })
-    // calculator =TestBed.get(CalculatorService)  Deprecate;
-    calculator =TestBed.inject(CalculatorService);
+    console.log("Before each called");
+    logger =jasmine.createSpyObj("LoggerService",["log"])
+
+    TestBed.configureTestingModule({
+      providers:[CalculatorService,{provide:LoggerService,useValue:logger}]
+    })
+    //calculator=new CalculatorService(logger);
+    // calculator=TestBed.get(CalculatorService);
+    calculator=TestBed.inject(CalculatorService);
+  })
+  fit("should subtract 2 numbers",()=>{
+
+    const result = calculator.subtract(3,2);
+    expect(result).withContext("should be").toBe(1);
   })
 
   it("should add 2 numbers",()=>{
-
-    // const logger=new LoggerService();
-    // spyOn(logger,"log");
+    // // const logger=new LoggerService();
+    // const logger =jasmine.createSpyObj("LoggerService",["log"])
+    //  //spyOn(logger,"log")
     // const calculator=new CalculatorService(logger);
     const result=calculator.add(2,5);
-    expect(result).withContext('should be').toBe(7)
+    expect(result).withContext("should be").toBe(7);
     expect(logger.log).toHaveBeenCalledTimes(1);
-    // expect(logger.log).withContext('should be called').toHaveBeenCalledTimes(1);
+    fail()
   });
-
-  it("should subtract 2 numbers",()=>{
-
-     // const logger=new LoggerService();
-    //  const logger =jasmine.createSpyObj('LoggerService',["log"]);
-    // const calculator=new CalculatorService(logger);
-    const result=calculator.subtract(3,2);
-    expect(result).withContext('should be').toBe(1);
-    expect(logger.log).toHaveBeenCalledTimes(1);
-
-
-  })
-
-
 
 })
